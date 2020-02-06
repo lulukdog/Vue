218275771e770d834703c61795859abd7681d88e
赵路 <zhaolu@kuaishou.com>
2020-02-06 15:27:54 +0800

    添加学习todolist

---
+++ Lesson/test-component/src/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+	<h1 class="green">h2...{{tt}}...title:{{title}}</h1>
+</template>
+
+<script>
+	export default {
+		name: "test",
+		props:['title'],
+		data(){
+			return {
+				tt: "this page",
+			}
+		}
+	}
+</script>
+
+<style>
+	.green{color: #8bff1e;}
+</style>
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Vue
 vue的工程
+
+
+
+[vue学习](https://cn.vuejs.org/)
